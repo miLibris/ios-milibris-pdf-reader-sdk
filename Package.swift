a5beec8e879a030b3b513ae0468dbee8fc5791fa
Maxime Le Moine <maxime.lemoine@milibris.com>
2020-11-19 19:07:02 +0100

    Fix download URL

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MLPDFReaderSDK",
-            url: "https://github.com/miLibris/ios-milibris-pdf-reader-sdk/releases/download/1.2.0/MLPDFReaderSDK.xcframework.zip",
+            url: "https://github.com/miLibris/ios-milibris-pdf-reader-sdk/releases/download/v1.2.0/MLPDFReaderSDK.xcframework.zip",
             checksum: "41b65d79ac5f1db1e0861fb4b286baf38e1ac52090c394fbfa526b0282609fba"
         ),
     ]
